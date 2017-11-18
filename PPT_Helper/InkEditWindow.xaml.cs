5bceb0006849bb5eda6bf8c4cdcf92a4d88b23ed
117503445 <t117503445@gmail.com>
2017-11-18 22:36:56 +0800

    demo1118_2236

---
+++ PPT_Helper/InkEditWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PPT_Helper
+{
+    /// <summary>
+    /// InkEditWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class InkEditWindow : Window
+    {
+        public InkEditWindow()
+        {
+            InitializeComponent();
+
+            drawingAttributes.Width = 5;
+            drawingAttributes.Height = 5;
+            drawingAttributes.Color = Colors.DeepSkyBlue;
+            drawingAttributes.FitToCurve = true;
+            
+        }
+        public DrawingAttributes drawingAttributes = new DrawingAttributes();
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (RdoBlack.IsChecked == true)
+            {
+                drawingAttributes.Color = Colors.Black;
+            }
+            else if (RdoBlue.IsChecked == true)
+            {
+                drawingAttributes.Color = Colors.DeepSkyBlue;
+            }
+            else if (RdoRed.IsChecked == true)
+            {
+                drawingAttributes.Color = Colors.Red;
+            }
+            else if (RdoWhite.IsChecked == true)
+            {
+                drawingAttributes.Color = Colors.White;
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Hide();
+            e.Cancel = true;
+        }
+
+
+        private void Slider_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+        {
+            drawingAttributes.Width = Slider.Value;
+            drawingAttributes.Height = Slider.Value;
+        }
+
+        private void Window_Activated(object sender, EventArgs e)
+        {
+            Top = SystemParameters.PrimaryScreenHeight - 300;
+            Left =( SystemParameters.PrimaryScreenWidth - Width) / 2;
+        }
+    }
+}
--- PPT_Helper/MainWindow.xaml.cs
+++ PPT_Helper/MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Runtime.InteropServices;
 using PPt = Microsoft.Office.Interop.PowerPoint;
+using System.Windows.Threading;
+
 namespace PPT_Helper
 {
     /// <summary>
@@ -38,6 +40,7 @@ namespace PPT_Helper
         /// 定义单个幻灯片对象
         /// </summary>
         PPt.Slide slide;
+        PPt.Slide lastslide;
         /// <summary>
         /// 幻灯片的数量
         /// </summary>
@@ -46,7 +49,11 @@ namespace PPT_Helper
         /// 幻灯片的索引
         /// </summary>
         int slideIndex;
-
+        /// <summary>
+        /// 每页PPT都有的INK
+        /// </summary>
+        InkCanvas[] inkCanvas;
+        InkEditWindow inkEditWindow=new InkEditWindow();
         public MainWindow()
         {
             InitializeComponent();
@@ -73,14 +80,40 @@ namespace PPT_Helper
                     // 在普通视图下这种方式可以获得当前选中的幻灯片对象
                     // 然而在阅读模式下，这种方式会出现异常
                     slide = slides[pptApplication.ActiveWindow.Selection.SlideRange.SlideNumber];
+                    lastslide = slide;
                 }
                 catch
                 {
                     // 在阅读模式下出现异常时，通过下面的方式来获得当前选中的幻灯片对象
                     slide = pptApplication.SlideShowWindows[1].View.Slide;
                 }
+
             }
+
+            inkCanvas = new InkCanvas[slidesCount];
+            for (int i = 0; i < inkCanvas.Length; i++)
+            {
+                inkCanvas[i] = new InkCanvas();
+            }
+            foreach (var item in inkCanvas)
+            {
+                item.Visibility = Visibility.Hidden;
+                var bc = new BrushConverter();
+                item.Background = (Brush)new BrushConverter().ConvertFrom("#02FFFFFF");
+                grid.Children.Add(item);
+                Grid.SetColumnSpan(item, 3);
+                Grid.SetRowSpan(item, 1);
+                item.DefaultDrawingAttributes = inkEditWindow.drawingAttributes;
+                item.StrokeCollected += Item_StrokeCollected;
+            }
+
         }
+
+        private void Item_StrokeCollected(object sender, InkCanvasStrokeCollectedEventArgs e)
+        {
+            inkEditWindow.Hide();
+        }
+
         private void BtnPrev_Click(object sender, RoutedEventArgs e)
         {
             slideIndex = slide.SlideIndex - 1;
@@ -102,7 +135,8 @@ namespace PPT_Helper
             {
                 MessageBox.Show("已经是第一页了");
             }
-
+            TbDebug.Text = slide.SlideIndex.ToString();
+            SwitchInk();
         }
         private void BtnNext_Click(object sender, RoutedEventArgs e)
         {
@@ -117,14 +151,107 @@ namespace PPT_Helper
                 {
                     slide = slides[slideIndex];
                     slides[slideIndex].Select();
+
                 }
                 catch
                 {
                     // 在阅读模式下使用下面的方式来切换到下一张幻灯片
                     pptApplication.SlideShowWindows[1].View.Next();
                     slide = pptApplication.SlideShowWindows[1].View.Slide;
+
                 }
             }
+            TbDebug.Text = slide.SlideIndex.ToString();
+            SwitchInk();
         }
+
+        private void SwitchInk()
+        {
+            foreach (var item in inkCanvas)
+            {
+                item.Visibility = Visibility.Hidden;
+            }
+            inkCanvas[slide.SlideIndex - 1].Visibility = Visibility.Visible;
+        }
+
+
+        private void ImgInk_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            foreach (var item in inkCanvas)
+            {
+                item.EditingMode = InkCanvasEditingMode.Ink;
+            }
+            eraserMode = false;
+            ImgMouse.Source = new BitmapImage(new Uri("/Resources/Tools/UnChecked/Mouse.jpg", UriKind.RelativeOrAbsolute));
+            ImgInk.Source = new BitmapImage(new Uri("/Resources/Tools/Checked/Pen.jpg", UriKind.RelativeOrAbsolute));
+            ImgEraser.Source= new BitmapImage(new Uri("/Resources/Tools/Unchecked/Eraser.jpg", UriKind.RelativeOrAbsolute));
+            SwitchInk();
+
+        }
+
+        private void ImgMouse_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            eraserMode = false;
+            ImgMouse.Source = new BitmapImage(new Uri("/Resources/Tools/Checked/Mouse.jpg", UriKind.RelativeOrAbsolute));
+            ImgInk.Source = new BitmapImage(new Uri("/Resources/Tools/UnChecked/Pen.jpg", UriKind.RelativeOrAbsolute));
+            ImgEraser.Source = new BitmapImage(new Uri("/Resources/Tools/Unchecked/Eraser.jpg", UriKind.RelativeOrAbsolute));
+            foreach (var item in inkCanvas)
+            {
+                item.Visibility = Visibility.Hidden;
+            }
+
+        }
+        /// <summary>
+        /// 是否在橡皮模式
+        /// </summary>
+        bool eraserMode = false;
+        private void ImgEraser_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            foreach (var item in inkCanvas)
+            {
+                item.EditingMode = InkCanvasEditingMode.EraseByStroke;
+            }
+            if (eraserMode)
+            {
+                if (MessageBox.Show("是否全部清理", "", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    inkCanvas[slide.SlideIndex - 1].Strokes.Clear();
+                }
+            }
+            else
+            {
+                ImgMouse.Source = new BitmapImage(new Uri("/Resources/Tools/UnChecked/Mouse.jpg", UriKind.RelativeOrAbsolute));
+                ImgInk.Source = new BitmapImage(new Uri("/Resources/Tools/UnChecked/Pen.jpg", UriKind.RelativeOrAbsolute));
+                ImgEraser.Source = new BitmapImage(new Uri("/Resources/Tools/checked/Eraser.jpg", UriKind.RelativeOrAbsolute));
+                eraserMode = true;
+            }
+
+        }
+        private void ImgClose_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+#if !DEBUG
+            if (MessageBox.Show("是否退出?", "退出", MessageBoxButton.YesNo) == MessageBoxResult.No)
+            {
+                return;
+            }
+#endif
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
+        }
+
+        private void ImgSetting_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (inkEditWindow.IsVisible)
+            {
+                inkEditWindow.Hide();
+            }
+            else
+            {
+                inkEditWindow.Show();
+                inkEditWindow.Topmost = true;
+            }
+
+        }
+
+
     }
 }
